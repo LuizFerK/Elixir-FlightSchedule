406ee3b0024bfb9abbe5eccdffa29b1bda1ab279
LuizFerK <luizfernandoklein@live.com>
2021-03-18 15:43:06 -0300

    chore: Credo

--- lib/flightex.ex
+++ lib/flightex.ex
@@ -1,17 +1,4 @@
 defmodule Flightex do
-  @moduledoc """
-  Documentation for `Flightex`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Flightex.hello()
-      :world
-
-  """
   def hello do
     :world
   end
--- test/flightex_test.exs
+++ test/flightex_test.exs
@@ -1,6 +1,5 @@
 defmodule FlightexTest do
   use ExUnit.Case
-  doctest Flightex
 
   test "greets the world" do
     assert Flightex.hello() == :world
